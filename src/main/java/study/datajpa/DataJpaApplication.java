0d0be8241a75d73344d9dd6e30fe7e6b8376211d
WoojinKim <dnwls488@naver.com>
2022-06-21 23:52:43 +0900

    Spring Auditing 테스트

--- src/main/java/study/datajpa/DataJpaApplication.java
+++ src/main/java/study/datajpa/DataJpaApplication.java
@@ -2,7 +2,14 @@ package study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 //@EnableJpaRepositories(basePackages = "study.datajpa.repository")
 public class DataJpaApplication {
@@ -10,5 +17,12 @@ public class DataJpaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DataJpaApplication.class, args);
 	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+//		SpringSessionContext
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
+
 
 }
